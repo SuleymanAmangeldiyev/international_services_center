526b5589de592e52e03896c1e832b5d204ee8fc6
Suleyman <suleyman20071998@gmail.com>
2023-06-23 11:04:11 +0300

    second test

--- project 1/src/App.jsx
+++ project 1/src/App.jsx
@@ -1,14 +1,14 @@
 // import { useState } from "react";
 // import Login from "./pages/log/login/login";
 // import Signup from "./pages/log/signup/Signup";
-import Home from "./pages/home/Home";
-// import Log from "./pages/log/Log";
+// import Home from "./pages/home/Home";
+import Log from "./pages/log/Log";
 
 function App() {
   return (
     <>
-      {/* <Log /> */}
-      <Home />
+      <Log />
+      {/* <Home /> */}
     </>
   );
 }
