504454f378cd25f8d3e554e1ad0c0cf8632fcca1
Suleyman <suleyman20071998@gmail.com>
2023-06-23 11:22:48 +0300

    seconds test

--- project 1/src/pages/log/Log.jsx
+++ project 1/src/pages/log/Log.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export default function Log() {
+  const log_header = <h1>International Services Center</h1>;
   return (
-    <div>Log</div>
-  )
+    <>
+      <div className="login">
+        {log_header}
+        <section>
+          <form>
+            <input type="text" placeholder="User name" />
+            <input type="password" placeholder="Password" />
+          </form>
+        </section>
+      </div>
+      {/* <div className="signup">{log_header}</div> */}
+    </>
+  );
 }
