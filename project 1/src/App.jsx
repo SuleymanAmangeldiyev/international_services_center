2a2e78a99e66078cc13a92cb8dbe84810b772370
Suleyman <suleyman20071998@gmail.com>
2023-07-05 16:03:10 +0300

    ad

--- project 1/src/App.jsx
+++ project 1/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react";
+ // import { useState } from "react";
 // import Login from "./pages/log/login/login";
 // import Signup from "./pages/log/signup/Signup";
 import Home from "./pages/home/Home";
--- project 1/src/pages/home/hopeP/Ghome.jsx
+++ project 1/src/pages/home/hopeP/Ghome.jsx
@@ -3,30 +3,63 @@ import "./ghome.css"
 
 export default function Ghome() {
   return (
-    <div className='ghome'>
-      <section className='ghome-header'>
+    <div className="ghome">
+      <section className="ghome-header">
         <div></div>
       </section>
-      <section className='ghome-second_gyra'>
+      <section className="ghome-second_gyra">
         <div></div>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
         <div></div>
-        <div></div>
-        <div></div>
-      </section>
-      <section>
         <div></div>
         <div></div>
       </section>
-      <section>
-        <div></div>
+      <section className="ghome-third_section">
+        <div className="ghome-thd-div_1">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className="ghome-thd-div_2">
+          <div className="ghome-thd-div_2-1"></div>
+          <div className="ghome-thd-div_2-1"></div>
+          <div className="ghome-thd-div_2-3"></div>
+        </div>
+      </section>
+      <section className="ghome-fou-grid">
+        <div className="ghome-fou-grid1"></div>
+        <div className="ghome-fou-grid2"></div>
+        <div className="ghome-fou-grid3"></div>
+        <div className="ghome-fou-grid4"></div>
+        <div className="ghome-fou-grid5"></div>
+        <div className="ghome-fou-grid6"></div>
+        <div className="ghome-fou-grid7"></div>
+        <div className="ghome-fou-grid8"></div>
+        <div className="ghome-fou-grid9"></div>
       </section>
       <section></section>
       <section></section>
       <section></section>
     </div>
-  )
+  );
 }
